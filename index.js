84c853098ad2ba5cb6d663b80913efae3c056682
Kayla Reopelle <kreopelle@gmail.com>
2018-02-20 18:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,27 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+        var cartItem = cart[i];
+        var cartItemKey = Object.keys(cartItem);
+        var cartItemPrice = Object.values(cartItem);
+        if (cart.length === 1) {
+          console.log(`In your cart, you have ${Object.keys(cart[i])} at $${Object.values(cart[i])}.`);
+    	  } else if (cart.length === 2) {
+          console.log(`In your cart, you have ${cartItemKey[0]} at $${cartItemPrice[0]} and ${Object.keys(cart[i+1])} at $${Object.values(cart[i+1])}.`);
+        } else {
+            var shoppingList = [];
+          for (var j = 0; j < cart.length-1; j++) {
+            shoppingList.unshift(`${cartItemKey[j]} at $${cartItemPrice[j]}, `);
+          }
+          console.log(`In your cart, you have ${shoppingList}.`);
+            return cart;
+        }
+    }
+  }
 }
 
 function total() {
