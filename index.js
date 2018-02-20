9a43ea28304188a71d5aee6b903923f15daa13fa
Kayla Reopelle <kreopelle@gmail.com>
2018-02-20 18:22:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,33 +9,36 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+	var itemPrice = Math.floor(Math.random() * Math.floor(100))
+	cart.unshift({[itemName]: itemPrice})
+	console.log(`${itemName} has been added to your cart.`)
+	return cart;
 }
 
 function viewCart() {
+  
   if (cart.length === 0) {
     console.log("Your shopping cart is empty.");
   } else {
-    for (var i = 0; i < cart.length; i++) {
+  for (var i = 0; i < cart.length-1; i++) {
         var cartItem = cart[i];
         var cartItemKey = Object.keys(cartItem);
         var cartItemPrice = Object.values(cartItem);
         if (cart.length === 1) {
-          console.log(`In your cart, you have ${Object.keys(cart[i])} at $${Object.values(cart[i])}.`);
+          console.log(`In your cart, you have ${cartItemKey} at $${cartItemPrice}.`);
     	  } else if (cart.length === 2) {
-          console.log(`In your cart, you have ${cartItemKey[0]} at $${cartItemPrice[0]} and ${Object.keys(cart[i+1])} at $${Object.values(cart[i+1])}.`);
+          console.log(`In your cart, you have ${cartItemKey} at $${cartItemPrice} and ${Object.keys(cart[i+1])} at $${Object.values(cart[i+1])}.`);
         } else {
-            var shoppingList = [];
-          for (var j = 0; j < cart.length-1; j++) {
-            shoppingList.unshift(`${cartItemKey[j]} at $${cartItemPrice[j]}, `);
-          }
+          var shoppingList = [];
+            shoppingList.unshift(`${cartItemKey[i]} at $${cartItemPrice[i]}, `);
           console.log(`In your cart, you have ${shoppingList}.`);
             return cart;
         }
     }
   }
 }
+
 
 function total() {
   // write your code here
