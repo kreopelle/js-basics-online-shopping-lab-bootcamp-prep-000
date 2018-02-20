16ee7ba41a7cd9bc2e5a41aaa097df6184123aec
Kayla Reopelle <kreopelle@gmail.com>
2018-02-20 18:31:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function viewCart() {
 function total() {
   var total = 0
   for (var i = 0; i < cart.length-1; i++) {
-    total+=Object.values(cart[i]);
+    total+=(cart[i]);
   }
   return total;
 }
